68253c7d87bd943da265ccd336139f136a0ebf91
KurozawaChiaki <h1kari@connect.hku.hk>
2024-10-26 19:08:21 +0800

    A3 P4
    Finish!🎉

--- COMP7404/a3/p4.py
+++ COMP7404/a3/p4.py
@@ -1,8 +1,26 @@
+"""
+The data has been hard coded in the program, thus just click run.
+However, if there is a demand of changing parameters, modifying variables in main() function is available.
+The policy calculated in each iteration will be printed in the terminal.
+"""
 import copy, random
 
 
 class QLearning:
-    def __init__(self, alpha, discount, noise, living_reward, grid, eps = 1e-6):
+    """
+    The main class of implementing Q Learning
+    """
+    def __init__(self, alpha, discount, noise, living_reward, grid, eps=1e-6):
+        """
+        Initialization of the instance
+
+        :param alpha:            Learning rate
+        :param discount:         Discount factor, which is the lambda in slides
+        :param noise:            Possibility of unintended moves (not for the epsilon-greedy!)
+        :param living_reward:    Living reward
+        :param grid:             Map of the game
+        :param eps:              seems to be useless, but maybe someday I need it to compare floats
+        """
         self.alpha = alpha
         self.discount = discount
         self.noise = noise
@@ -10,7 +28,7 @@ class QLearning:
         self.grid = grid
         self.eps = eps
         self.actions = {"N": (-1, 0), "E": (0, 1), "S": (1, 0), "W": (0, -1)}
-        self.directions = {'N':['N', 'E', 'W'], 'E':['E', 'S', 'N'], 'S':['S', 'W', 'E'], 'W':['W', 'N', 'S']}
+        self.directions = {'N': ['N', 'E', 'W'], 'E': ['E', 'S', 'N'], 'S': ['S', 'W', 'E'], 'W': ['W', 'N', 'S']}
         self.n = len(grid)
         self.m = len(grid[0])
         self.Q = [[{"N": 0.0, "E": 0.0, "S": 0.0, "W": 0.0, "x": 0.0} for _ in range(self.m)] for _ in range(self.n)]
@@ -18,17 +36,14 @@ class QLearning:
         self.start = (0, 0)
         self.exit_states = []
         self.symbols = {
-            "N": "↑",
-            "E": "→",
-            "W": "←",
-            "S": "↓",
-            "x": "x",
-            "#": "#",
-            " ": " "
-        }
-        self.epsilon = 0.6
-        self.exploration_count = 0
+            "N": "↑", "E": "→", "W": "←", "S": "↓",
+            "x": "x", "#": "#", " ": " "
+         }
+        self.epsilon = 0.6              # Here is the epsilon for epsilon-greedy algorithm
+        self.epsilon_decay = 0.995      # Decay rate for epsilon
+        self.alpha_decay = 0.995        # Decay rate for alpha (learning rate)
 
+        # Initialize important parameters
         for i in range(self.n):
             for j in range(self.m):
                 if grid[i][j] == '#':
@@ -39,34 +54,30 @@ class QLearning:
                     self.exit_states.append((i, j))
                     self.policy[i][j] = "x"
 
-        print(self.exit_states)
-
-
     def get_action(self, location):
+        """
+        Get the action for agent in iteration.
+
+        :param location:  the location of the agent now
+        :return:          the action agent should take next
+        """
         if location in self.exit_states:
             return "x"
 
-        if self.policy[location[0]][location[1]] != " ":
-            intend = self.policy[location[0]][location[1]]
-            random_action = list(self.actions.keys())
-            random_action.remove(intend)
-            exploration = random.choice(random_action)
+        # choose the "best" action according to Q values
+        best_action = self.get_new_policy(location, self.Q[location[0]][location[1]])
 
-            res = random.choices(
-                population = [intend, exploration],
-                weights = [1 - self.epsilon, self.epsilon])[0]
-            self.exploration_count += 1
-            if self.exploration_count >= 200:
-                self.exploration_count = 0
-                self.epsilon -= 0.05
-            return res
+        if random.uniform(0, 1) < self.epsilon:
+            available_actions = [action for action in self.actions]
+            chosen_action = random.choice(available_actions)
+        else:
+            chosen_action = best_action
 
-        return random.choice(list(self.actions.keys()))
-
+        self.epsilon *= self.epsilon_decay
+        return chosen_action
 
     def get_max_q(self, location):
         return max(self.Q[location[0]][location[1]].values())
-
 
     def print_policy(self):
         res = ""
@@ -77,42 +88,32 @@ class QLearning:
         res += "-----------------------\n"
         print(res[:-1])
 
-
-    def next_location(self, agent, d):
-        d = random.choices(
-            population = self.directions[d],
-            weights = [1 - 2 * self.noise, self.noise, self.noise]
+    def next_location(self, agent, intended_direction):
+        chosen_direction = random.choices(
+            population=self.directions[intended_direction],
+            weights=[1 - 2 * self.noise, self.noise, self.noise]
         )[0]
 
-        x = agent[0] + self.actions[d][0]
-        y = agent[1] + self.actions[d][1]
+        x, y = agent[0] + self.actions[chosen_direction][0], agent[1] + self.actions[chosen_direction][1]
 
-        if 0 <= x < self.n and 0 <= y < self.m:
-            if self.grid[x][y] != "#":
-                return x, y
-            elif self.grid[x][y] == "#":
-                return agent
+        if 0 <= x < self.n and 0 <= y < self.m and self.grid[x][y] != "#":
+            return x, y
+        return agent
 
-        if x < 0:
-            x = 0
-        if x >= self.n:
-            x = self.n - 1
+    def check_complete(self, policy):
+        for i in range(self.n):
+            for j in range(self.m):
+                if policy[i][j] != self.policy[i][j]:
+                    return False
+        return True
 
-        if y < 0:
-            y = 0
-        if y >= self.m:
-            y = self.m - 1
-
-        return x, y
-
-
-    def get_new_policy(self, location):
+    def get_new_policy(self, location, Q_cell):
         if self.policy[location[0]][location[1]] == "x":
             return "x"
 
         best_action = ""
         max_q = -float("inf")
-        for action, value in self.Q[location[0]][location[1]].items():
+        for action, value in Q_cell.items():
             if action == "x":
                 continue
             if value > max_q:
@@ -121,19 +122,17 @@ class QLearning:
 
         return best_action
 
-
     def solve(self):
         iteration = 0
-        delta = float("inf")
-        while delta > self.eps:
-            delta = 0.0
+        complete = False
+        while (not complete) or iteration < 1000:
             agent = self.start
             iteration += 1
             Q = copy.deepcopy(self.Q)
             policy = copy.deepcopy(self.policy)
             action = ""
             while action != "x":
-                # print(agent)
+                print(f"{action} {agent}")
                 action = self.get_action(agent)
                 value = 0.0
                 new_agent = copy.deepcopy(agent)
@@ -147,13 +146,14 @@ class QLearning:
 
                 Q[agent[0]][agent[1]][action] = ((1 - self.alpha) * self.Q[agent[0]][agent[1]][action]
                                                  + self.alpha * value)
-                policy[agent[0]][agent[1]] = self.get_new_policy(agent)
-                delta += abs(self.Q[agent[0]][agent[1]][action] - Q[agent[0]][agent[1]][action])
+                policy[agent[0]][agent[1]] = self.get_new_policy(agent, Q[agent[0]][agent[1]])
                 agent = new_agent
+                self.Q = Q
 
-            self.Q = Q
+            complete = self.check_complete(policy)
             self.policy = policy
             self.print_policy()
+            self.alpha *= self.alpha_decay
 
 
 def main():
